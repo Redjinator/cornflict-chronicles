a5bbcc9312f75c881148468425b7d881234f64b7
Redjinator <rmcpherson@northislandcollege.ca>
2023-02-08 15:32:26 -0800

    complete: loading images into the texture cache

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import './style.css'
 
+// Alias
+const Application = PIXI.Application,
+  loader = PIXI.Loader.shared,
+  resources = PIXI.Loader.shared.resources,
+  Sprite = PIXI.Sprite;
+
+
 /// Pixi App
-const app = new PIXI.Application({
+const app = new Application({
   width: 256,
   height: 256,
   antialias: true,
@@ -23,3 +30,21 @@ console.log(width, height);
 
 // Change the size of the canvas, set AutoDensity to true to make sure it matches the screen
 app.renderer.resize(1280, 720);
+
+// use loader
+loader
+  .add('farmer-v3.png')
+  .add('enemy.png')
+  .add('bullet.png')
+  .add('field-bg.png')
+  .load(setup);
+
+// Setup function
+function setup() {
+  // This code will run, when the loader finishes loading the images
+  const farmer = new Sprite(resources['farmer-v3.png'].texture);
+}
+
+
+
+
