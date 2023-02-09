fd4507e5395b1dec660080c108eef0da196bbd28
Redjinator <rmcpherson@northislandcollege.ca>
2023-02-08 19:25:26 -0800

    refactor: cleanup and added regions

--- main.js
+++ main.js
@@ -3,19 +3,19 @@
 import { keyboard } from './keyboard.js'
 import './myfunctions.js'
 import { setSpriteProperties, loadProgressHandler } from './myfunctions.js';
-
 import { Container, TextStyle } from 'pixi.js';
+import { hitTestRectangle } from './collisions.js';
 
-// Alias
+/* #region Aliases */
 const Application = PIXI.Application,
   loader = PIXI.Loader.shared,
   TextureCache = PIXI.utils.TextureCache,
   resources = PIXI.Loader.shared.resources,
   Text = PIXI.Text,
   Sprite = PIXI.Sprite;
+/* #endregion */
 
-
-/// Pixi App
+/* #region Pixi Application */
 const app = new Application({
   width: 1280,
   height: 720,
@@ -35,6 +35,7 @@ const { width, height } = app.view;
 
 // Log the width and height of the canvas
 console.log(width, height);
+/* #endregion */
 
 // Define any variables that are used in more than one function, making them available globally
 let farmer, enemy, fieldbg, bullet, id, state, score;
@@ -56,13 +57,11 @@ loader
 
 
 
-
+/* #region Setup */
 //----------------------
 // Setup function
 //----------------------
 function setup() {
-
-  app.stage.interactive = true;
 
   // This code will run, when the loader finishes loading the images
   console.log("All files loaded");
@@ -70,6 +69,7 @@ function setup() {
   // Set score to 0
   score = 0;
 
+  /* #region Create Sprites */
   // Alias called id for all the texture atlas frame id textures
   id = resources["images/mvp-spritesheet.json"].textures;
 
@@ -77,15 +77,14 @@ function setup() {
   fieldbg = setSpriteProperties(new Sprite(id["field-bg.png"]), 1, 1, 1280, 720);
   farmer = setSpriteProperties(new Sprite(id["farmer-v3.png"]), 0.5, 0.2, 400, 100);
 
-
   gameScene.addChild(fieldbg);
   gameScene.addChild(farmer);
 
   // Add the sprites to the stage
   app.stage.addChild(gameScene);
-  
+  /* #endregion */
 
-  //Capture the keyboard arrow keys
+  /* #region Capture the keyboard arrow keys */
   const left = keyboard(37),
         up = keyboard(38),
         right = keyboard(39),
@@ -106,7 +105,14 @@ function setup() {
   //Down
   down.press = () => {farmer.vy = 5; farmer.vx = 0;};
   down.release = () => { if (!up.isDown && farmer.vx === 0) { farmer.vy = 0; }};
+/* #endregion */
 
+  /* #region Create Scoreboard */
+  const scoreboard = new Text("Score:" + score, scoreBoardStyle);
+  scoreboard.anchor.set(0.5, 0.5);
+  scoreboard.position.set(gameScene.width / 2, 100);
+  gameScene.addChild(scoreboard);
+  /* #endregion */
 
   // Set the game state
   state = play;
@@ -114,36 +120,31 @@ function setup() {
   // Starts game loop - Calls gameLoop every tick
   app.ticker.add(delta => gameLoop(delta));
 
-  // Add a text scoreboard
-  const scoreboard = new Text("Score:" + score, scoreBoardStyle);
-  scoreboard.anchor.set(0.5, 0.5);
-  scoreboard.position.set(gameScene.width / 2, 100);
-  gameScene.addChild(scoreboard);
 }
+/* #endregion */
 
 
 
-//----------------------
-// Game loop function
-//----------------------
+/* #region Function gameLoop */
 function gameLoop(delta) {
 
   // Update the current game state:
   // Because gameLoop is calling state 60 times per second, it means play function will also run 60 times per second.
   state(delta);
 }
+/* #endregion */
 
-//----------------------
-// Play function
-//----------------------
+
+/* #region Function play*/
 function play(delta) {
   // Move the farmer
   farmer.x += farmer.vx;
   farmer.y += farmer.vy;
 }
+/* #endregion */
 
 
-
+/* #region Scoreboard Style */
 const scoreBoardStyle = new TextStyle({
   fontFamily: "Arial",
   fontSize: 36,
@@ -158,3 +159,4 @@ const scoreBoardStyle = new TextStyle({
   wordWrap: true,
   wordWrapWidth: 440
 });
+/* #endregion */
