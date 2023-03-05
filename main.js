666b471745bf7939bedd04aa873dd9cb1faa887f
Redjinator <rmcpherson@northislandcollege.ca>
2023-03-04 18:14:23 -0800

    refactor: player and enemies to modules

--- main.js
+++ main.js
@@ -17,6 +17,9 @@ import { setSpriteProperties } from './spriteProperties.js';
 import { loadProgressHandler } from './loadProgress.js';
 import { randomSpawnPoint } from './spawnPoint.js';
 import { createBackground } from './createBackground.js';
+import { createPlayer } from './player.js';
+import { createEnemy } from './enemy.js';
+import { moveEnemies } from './enemyMovement.js';
 
 
 const Application = PIXI.Application,
@@ -83,9 +86,17 @@ export function setup() {
   app.stage.interactive = true;
 
   // *Create the farmer
-  farmer  = setSpriteProperties(new Sprite(id["farmer-v3.png"]), 0.5, 0.2, 640, 360);
-  farmer = setupKeyboard(farmer);
+  farmer  = createPlayer(id);
+
+  // *Create the hearts container
   heartsContainer = createHearts(app);
+
+  // *Create the enemies
+/*   for (let i=0; i<enemyCount; i++) {
+    let enemy = createEnemy(id);
+    enemies.push(enemy);
+    gameScene.addChild(enemy);
+  } */
 
 
   // *Create the background
@@ -172,35 +183,19 @@ function play(delta) {
   updateBG(farmerDeltaX, farmerDeltaY);
 
 
+
+
+
+
   // *Enemy Respawning
   if (enemies.length < enemyCount) {
-    let enemy = new Sprite(id["enemy.png"]);
+    let enemy = createEnemy(id);
     enemies.push(enemy);
     gameScene.addChild(enemy);
-    enemy.scale.set(0.5, 0.5);
-    enemy.anchor.set(0.5, 0.5);
-    let r = randomSpawnPoint();
-    enemy.x = r.x;
-    enemy.y = r.y;
   }
-
 
   // *Move enemies
-  for (let i = 0; i < enemies.length; i++) {
-    let enemy = enemies[i];
-    let e = new Victor(enemy.x, enemy.y);
-    let f = new Victor(farmer.x, farmer.y);
-    let d = f.subtract(e);
-    let v = d.normalize().multiplyScalar(enemySpeed);
-    enemy.position.set(enemy.position.x + v.x - farmerDeltaX, enemy.position.y + v.y - farmerDeltaY);
-
-      if (hitTestRectangle(farmer, enemy)) {
-        gameScene.removeChild(enemy);
-        enemies.splice(i, 1);
-        heartsContainer.removeChildAt(heartsContainer.children.length - 1);
-        break;
-      }
-  }
+  moveEnemies(enemies, farmer, farmerDeltaX, farmerDeltaY, enemySpeed, heartsContainer, gameScene);
 
 
   // *Bullet movement
@@ -231,13 +226,10 @@ function play(delta) {
 }
 
 
-
 // ! END FUNCTION
 function end() {
   scoreboard.resetScore();
 }
-
-
 
 function menu() {
 
