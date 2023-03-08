c3949285f6ac642e9a4b78c1bda9075914131f87
Redjinator <rmcpherson@northislandcollege.ca>
2023-03-07 21:07:36 -0800

    remove: solid fill bg

--- titlescreen.js
+++ titlescreen.js
@@ -4,20 +4,12 @@ export default class TitleScreen {
   constructor(app, startGame, id ) {
     this.titleScene = new Container();
 
-
-    // *Just a black background for now, will replace later with a cool title screen
-    const blackBG = new Graphics();
-    blackBG.beginFill(0x000000);
-    blackBG.drawRect(0, 0, app.view.width, app.view.height);
-    blackBG.endFill();
-    this.titleScene.addChild(blackBG); 
-
     const titleImage = new Sprite(id["title.png"]);
-
     titleImage.anchor.set(0.5);
     titleImage.position.set(app.view.width / 2, app.view.height / 2);
     this.titleScene.addChild(titleImage);
 
+    // #region Title Text
     const titleText = new Text('Cornflict Chronicles', {
       fontFamily: 'Arial',
       fontSize: 72,
@@ -28,21 +20,24 @@ export default class TitleScreen {
       dropShadowColor: "#000000",
       dropShadowBlur: 4,
       dropShadowAngle: Math.PI / 6,
-      dropShadowDistance: 12,
+      dropShadowDistance: 5,
       wordWrap: true,
       wordWrapWidth: 900,
       align: 'center',
     });
+
     titleText.position.set(app.view.width / 3, app.view.height / 1.8);
     titleText.anchor.set(0.5, 0.5);
     this.titleScene.addChild(titleText);
+    // #endregion
 
+    // #region Start Button Text
     const startButton = new Text('START GAME', {
       fontFamily: 'Arial',
       fontSize: 24,
       fill: "white",
       stroke: 'black',
-      strokeThickness: 4,
+      strokeThickness: 2,
       wordWrap: true,
       wordWrapWidth: 440,
       align: 'center',
@@ -50,6 +45,7 @@ export default class TitleScreen {
     startButton.anchor.set(0.5, 0.5);
     startButton.position.set(app.view.width / 2.2, app.view.height / 1.5);
     this.titleScene.addChild(startButton);
+    // #endregion
 
     this.music = new Audio('/audio/CreepyFX_LongTransition.mp3');
     this.music.loop = false;
@@ -57,9 +53,8 @@ export default class TitleScreen {
 
     startButton.interactive = true;
     startButton.buttonMode = true;
+
     startButton.on('pointerdown', () => {
-      let sound = new Audio('/audio/shot.mp3');
-      //sound.play();
       this.music.pause();
       this.titleScene.visible = false;
       startGame();
