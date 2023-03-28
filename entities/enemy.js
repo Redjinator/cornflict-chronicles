262bff9842892e31113a76f6a421bea0f8186d6d
Redjinator <rmcpherson@northislandcollege.ca>
2023-03-27 20:29:37 -0700

    Refactor: random enemy and readability

--- entities/enemy.js
+++ entities/enemy.js
@@ -1,16 +1,13 @@
-import { setSpriteProperties } from '../helpers/spriteProperties.js';
 import { Sprite } from 'pixi.js';
-
 export class Enemy extends Sprite {
-  constructor(id, speed, farmer, heartsContainer, gameScene) {
-    super(id['ecorn.png']);
-    setSpriteProperties(this, 0.5, 0.5, 0, 0);
+  constructor(id, speed) {
+    let enemy = Math.random() > 0.5 ? 'ecorn' : 'ecorn_lit';
+    super(id[enemy])
     this.scale.set(0.1);
     this.anchor.set(0.5);
-
+    this.position.set(0, 0);
+    this.vx = 0;
+    this.vy = 0;
     this.speed = Math.random() * speed;
-    this.farmer = farmer;
-    this.heartsContainer = heartsContainer;
-    this.gameScene = gameScene;
   }
 }
