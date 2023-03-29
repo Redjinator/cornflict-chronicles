60e5aa904f5bacea3c4aa47ce3b77b82e332a922
Redjinator <rmcpherson@northislandcollege.ca>
2023-03-28 19:04:39 -0700

    Refactor: bullet exit position adjustment

--- entities/shoot.js
+++ entities/shoot.js
@@ -1,5 +1,3 @@
-import { createBullet } from './bullet.js';
-
 export function shoot(farmer, bullets, gameScene) {
   for (let i=0; i<bullets.length; i++) {
     let bullet = bullets[i];
@@ -7,11 +5,18 @@ export function shoot(farmer, bullets, gameScene) {
       bullet.x = farmer.x;
       bullet.y = farmer.y;
       bullet.rotation = (farmer.rotation + Math.PI / 2);
-      const speed = 10;
+      const speed = 15;
       bullet.vx = Math.cos(farmer.rotation) * speed;
       bullet.vy = Math.sin(farmer.rotation) * speed;
       bullet.zIndex = 0;
+      bullet.alpha = 0;
+      farmer.zIndex = 1;
       gameScene.addChild(bullet);
+
+      setTimeout(() => {
+        bullet.alpha = 1;
+      }, 70);
+
       let instance = new Audio("/audio/shot.mp3");
       instance.volume = 0.1;
       instance.play();
