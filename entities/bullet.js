f9fd0693bdec82b161f765208fc824f25be4547f
Redjinator <rmcpherson@northislandcollege.ca>
2023-03-28 19:03:29 -0700

    Refactor: dry

--- entities/bullet.js
+++ entities/bullet.js
@@ -3,6 +3,7 @@ import { setSpriteProperties } from '../helpers/spriteProperties';
 
 export function createBullet(id) {
   let bullet  = setSpriteProperties(new Sprite(id["bullet"]), 0.5, 0.03, 0, 0);
+
   bullet.anchor.set(0.5, 0.5);
   bullet.rotation = 0.5
   bullet.zIndex = 0.5;
@@ -11,12 +12,4 @@ export function createBullet(id) {
   return bullet;
 }
 
-export function createAutoFireForks(id0) {
-  let forks = [];
-  for (let i=0; i<10; i++) {
-    let fork = setSpriteProperties(PIXI.Texture.from('images/pitchfork.png'), 0.5, 0.2, 640, 360).Texture;
-    fork.visible = false;
-    forks.push(fork);
-  }
-  return forks;
-}
+
