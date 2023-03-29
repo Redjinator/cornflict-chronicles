e68eb9d99dd36a6d76b6e338a3e2743e71e12875
Redjinator <rmcpherson@northislandcollege.ca>
2023-03-28 17:25:30 -0700

    Feat: single source of filters for test

---
+++ imports.js
@@ -0,0 +1,21 @@
+import * as PIXI from "pixi.js";
+
+// Pixi-filters
+import {
+    DropShadowFilter,
+    GlowFilter,
+    ShockwaveFilter,
+    GodrayFilter,
+    ReflectionFilter,
+    BloomFilter } from "pixi-filters";
+
+// Re-export
+export {
+    PIXI,
+    DropShadowFilter,
+    GlowFilter,
+    ShockwaveFilter,
+    GodrayFilter,
+    ReflectionFilter,
+    BloomFilter,
+};
