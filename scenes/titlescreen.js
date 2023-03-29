3a6aba7a093cc6bffbe07bd0b55e7a65dff7fcd8
Redjinator <rmcpherson@northislandcollege.ca>
2023-03-28 19:05:19 -0700

    Feat: visual effects

--- scenes/titlescreen.js
+++ scenes/titlescreen.js
@@ -1,5 +1,13 @@
 import { Container, Text, Sprite, Loader, Graphics } from 'pixi.js';
+import { GodrayFilter } from "pixi-filters";
 
+const godrayFilter = new GodrayFilter({
+  parallel: true,
+  lacunarity: 3,
+  angle: 45,
+  gain: 0.5,
+  alpha: 1,
+});
 export default class TitleScreen {
   constructor(app, startGame, id ) {
     this.titleScene = new Container();
@@ -10,7 +18,13 @@ export default class TitleScreen {
     titleImage.position.set(app.view.width / 2, app.view.height / 2);
     this.titleScene.addChild(titleImage);
 
+    this.titleScene.filters = [ godrayFilter ];
 
+    app.ticker.add((delta) => {
+      godrayFilter.time += 0.01 * delta;
+    });
+
+    
 
 
     const startButton = new Text('START GAME', {
@@ -42,3 +56,5 @@ export default class TitleScreen {
     });
   }
 }
+
+
